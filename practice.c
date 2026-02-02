8647a81c9a2385e98f7b17a9e36fc66d576eb3fa
Your Namenayandeep-09 <nayandeepchouhan864@gmail.com>
2026-02-02 16:07:05 +0530

    Added file handling program and updated practice code

--- practice.c
+++ practice.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 
-int main() {
-    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int b[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int c[3][3];
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            c[i][j] = 0;
-            for (int k = 0; k < 3; k++) {
-                c[i][j] = c[i][j] + (a[i][k] * b[k][j]);
-            }
-        }
-    }
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", c[i][j]);
-        }
-        printf("\n");
-    }
-
+int main(){
+    
     return 0;
 }
